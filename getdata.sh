35447fc62c1070bdde747372f6e19e068397fdce
milovanderlinden <milo@dogodigi.net>
2017-12-11 00:37:28 +0100

    Projects page

--- getdata.sh
+++ getdata.sh
@@ -1,3 +1,3 @@
-wget http://api.codeforamerica.org/api/organizations?type=Code+for+All -O ./_data/organizations.json
-wget http://api.codeforamerica.org/api/projects?organization_type=Code+for+All -O ./_data/projects.json
-wget http://api.codeforamerica.org/api/organizations.geojson -O ./assets/organizations.geojson
+wget "http://api.codeforamerica.org/api/organizations?type=Code+for+All&per_page=1000" -O ./_data/organizations.json
+wget "http://api.codeforamerica.org/api/projects?organization_type=Code+for+All" -O ./_data/projects.json
+wget "http://api.codeforamerica.org/api/organizations.geojson" -O ./assets/organizations.geojson
