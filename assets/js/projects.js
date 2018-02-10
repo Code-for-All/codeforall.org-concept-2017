9d912b5f2da6b1a6d91661eb90a0affa235350e3
milovanderlinden <milo@dogodigi.net>
2018-02-10 20:17:18 +0100

    Getting project from cfapi #38

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,47 +1,73 @@
-$(document).ready(function() {
+$(document).ready(function () {
   initDataTable();
-  function getProjects() {
-    $.getJSON('http://api.codeforamerica.org/api/organizations?type=Code+for+All&per_page=100', function(response) {
-      callback(response);
-    });
-  }
-  // Resize iframe to fit content
-  function resize() {
-    var newheight = document.getElementById("widget").contentWindow.document.body.scrollHeight;
-    document.getElementById("widget").height = newheight + "px";
-  }
+  // function getProjects() {
+  //   $.getJSON('http://api.codeforamerica.org/api/organizations?type=Code+for+All&per_page=100', function(response) {
+  //     callback(response);
+  //   });
+  // }
 
-  function initDataTable(){
+  function initDataTable() {
     $('#cfa-listings').dataTable({
       "ajax": function (data, callback, settings) {
-        settings.sAjaxDataProp = "objects";
-        $.getJSON('http://api.codeforamerica.org/api/organizations?type=Code+for+All&per_page=100', function(response) {
-          callback(response);
+        settings.sAjaxDataProp = "";
+        $.getJSON('http://api.codeforamerica.org/api/organizations?type=Code+for+All&per_page=100', function (response) {
+          // Process them first to get valid projects only
+          var projects = [];
+          response.objects.forEach(function (object) {
+            var valid_projects = [];
+            object.current_projects.forEach(function (project) {
+              if (project.description && project.description !== "") {
+                valid_projects.push(project);
+              }
+            })
+
+            if (valid_projects.length > 1) {
+              projects = projects.concat(valid_projects);
+            }
+          });
+          callback(projects);
         });
       },
-      "responsive": true,
+      "responsive": false,
       "searching": false,
-      "info": false,
-      "processing": true,
+      "info": true,
+      "processing": false,
       "serverSide": false,
-      "paging": false,
-      "columns": [
-        { "data": "name", "render": function (data, type, full, meta)
-          {
-            return '<a href="'+full.website+'">'+data+'</a>';
+      "paging": true,
+      "columns": [{
+          "data": "commit_status",
+          "render": function (data, type, full, meta) {
+            if(data){
+              switch (data.toLowerCase()) {
+                case "success":
+                  return '<i class="fas fa-rotate-270 fa-battery-full success" title="Launched"></i>';
+                case "pending":
+                  return '<i class="fas fa-rotate-270 fa-battery-half progress" title="In progress"></i>';
+                case "failure":
+                  return '<i class="fas fa-ban failure" title="Stopped"></i>';
+                default:
+                  return '<i class="fas fa-rotate-270 fa-battery-empty" title="' + data + '"></i>';
+              }
+            } else {
+              return '<i class="fas fa-rotate-270 fa-battery-empty unknown" title="' + data + '"></i>';
+            }
           }
         },
         {
-          "width": "50%",
-          "data": "current_projects.0.name",
-          "defaultContent": "...",
+          "data": "name",
           "render": function (data, type, full, meta) {
-            if (full.current_projects.length) {
-              return '<a href="'+full.current_projects[0].code_url+'">'+data+'</a><br /><small>'+full.current_projects[0].description+'</small>';
-            }
+            return '<a href="' + full.code_url + '">' + data + '</a>';
+          }
+        },
+        {
+          "data": "description"
+        },
+        {
+          "data": "organization_name",
+          "render": function (data, type, full, meta) {
+            return '<a href="/partners/' + data.toLowerCase().replace(/ /g, "-") + '">' + data + '</a>';
           }
         }
-
       ]
     });
   }
